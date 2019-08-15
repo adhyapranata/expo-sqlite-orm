6672558aa7c94e481bdd1498836d5087050d3531
Adhya Pranata <adhyapranata@gmail.com>
2019-08-15 11:45:44 +0700

    update sycned_at to synced_at

--- src/BaseModel.js
+++ src/BaseModel.js
@@ -12,7 +12,7 @@ export default class BaseModel {
     this.setProperties(obj)
     return new Proxy(this, this)
   }
-  
+
   setProperties(props) {
     const cm = this.constructor.columnMapping
     Object.keys(cm).forEach(k => {
@@ -26,7 +26,7 @@ export default class BaseModel {
     })
     return this
   }
-  
+
   static normalizeProps(props) {
     const obj = {}
     const cm = this.columnMapping
@@ -37,54 +37,54 @@ export default class BaseModel {
         obj[k] = null
       }
     })
-    
+
     return obj
   }
-  
+
   get(target, prop) {
     return this[prop] || typeof this[prop] === 'function' ? this[prop] : ''
   }
-  
+
   static get database() {
     throw new Error('Database não definida')
   }
-  
+
   static get repository() {
     return new Repository(this.database, this.tableName, this.columnMapping)
   }
-  
+
   static get tableName() {
     throw new Error('tableName não definido')
   }
-  
+
   static get childrens() {
     return []
   }
-  
+
   static get parent() {
     return null
   }
-  
+
   static get columnMapping() {
     return {}
   }
-  
+
   static createTable() {
     return this.repository.createTable()
   }
-  
+
   static dropTable() {
     return this.repository.dropTable()
   }
-  
+
   static create(obj) {
     return this.repository.insert(obj).then(res => new this(res))
   }
-  
+
   static update(obj) {
     return this.repository.update(obj).then(res => new this(res))
   }
-  
+
   save() {
     if (this.id) {
       return this.constructor.repository
@@ -96,11 +96,11 @@ export default class BaseModel {
         .then(res => this.setProperties(res))
     }
   }
-  
+
   destroy() {
     return this.constructor.repository.destroy(this.id)
   }
-  
+
   static destroy(id, options = {softDelete: false}) {
     if (this.columnMapping.hasOwnProperty('deleted_at') && options.softDelete) {
       return this.find(id).then(res => {
@@ -108,56 +108,56 @@ export default class BaseModel {
         return res.save()
       })
     }
-    
+
     return this.repository.destroy(id)
   }
-  
+
   static destroyAll() {
     return this.repository.destroyAll()
   }
-  
+
   static all() {
     return this.queryRaw()
   }
-  
+
   static count({ column = 'id', where }) {
     return this.queryRaw({options: {
       select: `COUNT(${column})`,
       where
     }})
   }
-  
+
   static avg({ column, where }) {
     return this.queryRaw({options: {
       select: `AVG(${column})`,
       where
     }})
   }
-  
+
   static sum({ column, where }) {
     return this.queryRaw({options: {
       select: `SUM(${column})`,
       where
     }})
   }
-  
+
   static find(id) {
     return this.repository.find(id).then(res => (res ? new this(res) : res))
   }
-  
+
   static findBy(where) {
     return this.repository
       .findBy(where)
       .then(res => (res ? new this(res) : res))
   }
-  
+
   /**
    * @param {columns: '*', page: 1, limit: 30, where: {}, order: 'id DESC'} options
    */
   static query(options) {
     return this.repository.query(options)
   }
-  
+
   static queryRaw({options = {}, tableName}) {
     let joinStatement = options.join ? options.join : ''
     let whereStatement = options.where ? `WHERE (${options.where})` : ''
@@ -165,40 +165,40 @@ export default class BaseModel {
     let sortStatement = options.order ? `ORDER BY ${options.order}` : ''
     let limitStatement = (options.limit && options.page) ? `LIMIT ${options.limit} OFFSET ${options.limit * (options.page - 1)}` : ''
     const table = tableName || this.tableName
-    
+
     const query = `SELECT ${options.select || '*'} FROM ${table} ${joinStatement} ${whereStatement} ${groupStatement} ${sortStatement} ${limitStatement};`
     const databaseLayer = new DatabaseLayer(this.database, this.tableName)
     return databaseLayer.executeSql(query).then(res => {
       return res.rows.length ? res.rows : []
     })
   }
-  
+
   static queryStringRaw(query, params) {
     const databaseLayer = new DatabaseLayer(this.database, this.tableName)
     return databaseLayer.executeSql(query, params).then(res => {
       return res.rows.length ? res.rows : []
     })
   }
-  
+
   hasMany(target, foreignKey = 'id', otherKey) {
     const self = Object.getPrototypeOf(this).constructor
     const key = otherKey || `${toUnderscoreCase(self.name)}_id`
     const options = {
       where: `${key} = ${this[foreignKey]}`
     }
-    
+
     return target.queryRaw({options})
   }
-  
+
   belongsToMany(target, foreignKey, otherKey = 'id') {
     const key = foreignKey || `${toUnderscoreCase(target.name)}_id`
     const options = {
       where: `${otherKey} = ${this[key]}`
     }
-    
+
     return target.queryRaw({options})
   }
-  
+
   hasOne(target, foreignKey = 'id', otherKey) {
     return this.hasMany(target, foreignKey, otherKey).then(res => {
       return res[0]
@@ -206,7 +206,7 @@ export default class BaseModel {
       throw err
     })
   }
-  
+
   belongsTo(target, foreignKey, otherKey = 'id') {
     return this.belongsToMany(target, foreignKey, otherKey).then(res => {
       return res[0]
@@ -214,45 +214,45 @@ export default class BaseModel {
       throw err
     })
   }
-  
+
   static pullAll({api, method = 'getAll', params = {}, queryOptions = {page: 1, limit: 15, order: 'id'}}) {
     if (!api) return false
     return api[toCamelCase(this.tableName)][method]({params}).then(res => {
       const data = res.data.data.map(item => {
-        item['sycned_at'] = moment().format('YYYY-MM-DD HH:mm:ss')
+        item['synced_at'] = moment().format('YYYY-MM-DD HH:mm:ss')
         return item
       })
-      
+
       const altered = this.alterResponse({data, method: 'pullAll'})
-      
+
       const databaseLayer = new DatabaseLayer(this.database, this.tableName)
       return databaseLayer.bulkInsertOrReplace(altered).then(res => {
         return this.query(queryOptions)
       })
     })
   }
-  
+
   static pull({api, method = 'get', id, params = {}}) {
     if (!api || !id) return false
     return api[toCamelCase(this.tableName)][method]({id, params}).then(res => {
       const data = res.data.data
-      data['sycned_at'] = moment().format('YYYY-MM-DD HH:mm:ss')
+      data['synced_at'] = moment().format('YYYY-MM-DD HH:mm:ss')
       const altered = this.alterResponse({data, method: 'pull'})
-      
+
       const databaseLayer = new DatabaseLayer(this.database, this.tableName)
       return databaseLayer.bulkInsertOrReplace([altered]).then(res => {
         return this.find(res[0].insertId)
       })
     })
   }
-  
+
   static getQueue() {
     const databaseLayer = new DatabaseLayer(this.database, this.tableName)
     return databaseLayer.executeSql(`SELECT *
                                      FROM ${this.tableName}
-                                     WHERE (sycned_at IS NULL OR updated_at > sycned_at OR deleted_at IS NOT NULL);`)
+                                     WHERE (synced_at IS NULL OR updated_at > synced_at OR deleted_at IS NOT NULL);`)
   }
-  
+
   static pushAll({api, methods = {store: 'store', update: 'update', destroy: 'destroy'}}) {
     if (!api) return false
     return this.getQueue()
@@ -260,7 +260,7 @@ export default class BaseModel {
         let instance = null
         let promise = Promise.resolve(null)
         if (!res.rows.length) return promise
-        
+
         for (let item of res.rows) {
           instance = new this(item)
           if (instance._isNew()) {
@@ -275,30 +275,30 @@ export default class BaseModel {
             promise = promise.then(() => instance.push({api, method: methods.destroy, payload: {id: item.id}}))
           }
         }
-        
+
         return promise
       }).catch(err => {
         throw err
       })
   }
-  
+
   push({api, method, payload}) {
     if (!api || !method || !payload) return false
     let data = null
     const self = Object.getPrototypeOf(this).constructor
-    
+
     payload.options = self.injectOptions({data: this})
-    
+
     return api[toCamelCase(self.tableName)][method](payload).then(res => {
       data = res.data.data
-      
+
       if (method === 'destroy') {
         return self.destroy(payload.id)
       }
-      
-      data['sycned_at'] = moment().format('YYYY-MM-DD HH:mm:ss')
+
+      data['synced_at'] = moment().format('YYYY-MM-DD HH:mm:ss')
       data = self.alterResponse({data, method: 'push'})
-      
+
       const databaseLayer = new DatabaseLayer(self.database, self.tableName)
       return databaseLayer.bulkInsertOrReplace([data]).then(res => {
         if (method === 'store') {
@@ -319,54 +319,54 @@ export default class BaseModel {
             throw err
           })
         }
-        
+
         return Promise.resolve(res)
       })
     }).catch(err => {
       if (err.response && err.response.status === 404) {
         return self.destroy(payload.body ? payload.body.id : payload.id)
       }
-      
+
       throw err
     })
   }
-  
+
   _isNew() {
-    return !this.sycned_at
+    return !this.synced_at
   }
-  
+
   _isUpdated() {
-    return moment(this.updated_at).isAfter(this.sycned_at)
+    return moment(this.updated_at).isAfter(this.synced_at)
   }
-  
+
   _isDestroyed() {
     return !!this.deleted_at
   }
-  
+
   _isNotSynced() {
     return this._isNew() || this._isUpdated() || this._isDestroyed()
   }
-  
+
   static isNew({item}) {
-    return !item.sycned_at
+    return !item.synced_at
   }
-  
+
   static isUpdated({item}) {
-    return moment(item.updated_at).isAfter(item.sycned_at)
+    return moment(item.updated_at).isAfter(item.synced_at)
   }
-  
+
   static isDestroyed({item}) {
     return !!item.deleted_at
   }
-  
+
   static isNotSynced({item}) {
     return this.isNew({item}) || this.isUpdated({item}) || this.isDestroyed({item})
   }
-  
+
   static injectOptions({data}) {
     return {}
   }
-  
+
   static alterResponse({data, method}) {
     return data
   }
